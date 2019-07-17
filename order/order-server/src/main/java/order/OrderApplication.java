f2998be2b3b2875d6d8da652c9c6742db838ab2d
gaozhao <gaozhao@ec.e-chemchina.com>
2019-07-17 13:59:28 +0800

    springcloud bus 整合

--- order/order-server/src/main/java/order/OrderApplication.java
+++ order/order-server/src/main/java/order/OrderApplication.java
@@ -2,14 +2,12 @@ package order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.imooc.product.client")
-@RefreshScope
 public class OrderApplication {
 
     public static void main(String[] args) {
